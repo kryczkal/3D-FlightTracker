fbe9e7e6be531eda66b61818cacd9cb2dc872082
Łukasz Kryczka <elek123456789101112@gmail.com>
2024-03-28 09:03:28 +0100

    Working basic shaders

--- 3D-FlightTracker-App/FlightTrackerApp.cs
+++ 3D-FlightTracker-App/FlightTrackerApp.cs
@@ -1,3 +1,6 @@
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Graphics;
+
 namespace _3D_FlightTracker_App;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
@@ -5,6 +8,17 @@ using OpenTK.Windowing.GraphicsLibraryFramework;
 
 public class FlightTrackerApp : GameWindow
 {
+    private float[] vertices =
+    {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f
+    };
+    int vertexBufferObject;
+    int vertexArrayObject;
+
+    Shader shader;
+
     public FlightTrackerApp(int width, int height) : base(GameWindowSettings.Default,
         new NativeWindowSettings()
         {
@@ -16,7 +30,22 @@ public class FlightTrackerApp : GameWindow
     protected override void OnLoad()
     {
         base.OnLoad();
-        // Initialization code goes here
+
+        // Load the shaders
+        shader = new Shader("shader.vert", "shader.frag");
+
+        // Set the clean buffer color
+        GL.ClearColor(0.3f, 0.1f, 0.6f, 1.0f);
+
+        // Generate a vertex buffer object
+        vertexBufferObject = GL.GenBuffer();
+        vertexArrayObject = GL.GenVertexArray();
+
+        GL.BindVertexArray(vertexArrayObject);
+        GL.BindBuffer(BufferTarget.ArrayBuffer, vertexBufferObject);
+        GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+        GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+        GL.EnableVertexAttribArray(0);
     }
 
     protected override void OnRenderFrame(FrameEventArgs e)
@@ -24,7 +53,38 @@ public class FlightTrackerApp : GameWindow
         base.OnRenderFrame(e);
         // Drawing code goes here
 
+        // Clean the screen with the color set in OnLoad
+        GL.Clear(ClearBufferMask.ColorBufferBit);
+
+        GL.UseProgram(0);
+        shader.Use();
+        GL.BindVertexArray(vertexArrayObject);
+        GL.DrawArrays(PrimitiveType.Triangles, 0 ,3);
+
         // Swap the front and back buffers to present the new frame
         SwapBuffers();
+    }
+
+    protected override void OnUpdateFrame(FrameEventArgs args)
+    {
+        base.OnUpdateFrame(args);
+
+        if (KeyboardState.IsKeyDown(Keys.Escape))
+        {
+            Close();
+        }
+    }
+
+    protected override void OnFramebufferResize(FramebufferResizeEventArgs e)
+    {
+        base.OnFramebufferResize(e);
+
+        GL.Viewport(0, 0, e.Width, e.Height);
+    }
+
+    protected override void OnUnload()
+    {
+        base.OnUnload();
+        shader.Dispose();
     }
 }
---
+++ 3D-FlightTracker-App/Shader.cs
@@ -0,0 +1,100 @@
+
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace _3D_FlightTracker_App;
+
+public class Shader : IDisposable
+{
+    private int Handle;
+    private bool IsDisposed = false;
+
+    public Shader(string vertexPath, string fragmentPath)
+    {
+        /*
+         * Compile the vertex and fragment shaders
+         */
+
+        int VertexShader;
+        int FragmentShader;
+
+        // Load the source code of the shaders
+        string VertexShaderSource = File.ReadAllText(vertexPath);
+        string FragmentShaderSource = File.ReadAllText(fragmentPath);
+
+        VertexShader = GL.CreateShader(ShaderType.VertexShader);
+        GL.ShaderSource(VertexShader, VertexShaderSource);
+
+        FragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+        GL.ShaderSource(FragmentShader, FragmentShaderSource);
+
+        // Compile the shaders
+        GL.CompileShader(VertexShader);
+
+        GL.GetShader(VertexShader, ShaderParameter.CompileStatus, out int statusCode);
+        if (statusCode == 0)
+        {
+            throw new Exception(GL.GetShaderInfoLog(VertexShader));
+        }
+
+        GL.CompileShader(FragmentShader);
+
+        GL.GetShader(FragmentShader, ShaderParameter.CompileStatus, out statusCode);
+        if (statusCode == 0)
+        {
+            throw new Exception(GL.GetShaderInfoLog(FragmentShader));
+        }
+
+        // Create the shader program
+        Handle = GL.CreateProgram();
+
+        // Attach the shaders to the program
+        GL.AttachShader(Handle, VertexShader);
+        GL.AttachShader(Handle, FragmentShader);
+
+        // Link the program
+        GL.LinkProgram(Handle);
+
+        GL.GetProgram(Handle, GetProgramParameterName.LinkStatus, out statusCode);
+        if (statusCode == 0)
+        {
+            throw new Exception(GL.GetProgramInfoLog(Handle));
+        }
+
+        // Clean up the shaders
+        GL.DetachShader(Handle, VertexShader);
+        GL.DetachShader(Handle, FragmentShader);
+        GL.DeleteShader(VertexShader);
+        GL.DeleteShader(FragmentShader);
+    }
+
+    public void Use()
+    {
+        GL.UseProgram(Handle);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!IsDisposed)
+        {
+            GL.DeleteProgram(Handle);
+            IsDisposed = true;
+        }
+    }
+
+    ~Shader()
+    {
+        if (!IsDisposed)
+        {
+            throw new Exception("Shader was not disposed properly.");
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+}
