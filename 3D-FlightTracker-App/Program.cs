2e6439af6a756110605742b09b338483f700a9b6
Łukasz Kryczka <elek123456789101112@gmail.com>
2024-03-28 09:07:11 +0100

    Working Triangle Rendering

---
+++ 3D-FlightTracker-App/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _3D_FlightTracker_App;
+
+static class Program
+{
+    public static void Main()
+    {
+        using (FlightTrackerApp app = new FlightTrackerApp(1280, 720))
+        {
+            Console.WriteLine("Starting 3D Flight Tracker...");
+            app.Run();
+            Console.WriteLine("3D Flight Tracker closed.");
+        }
+    }
+}
